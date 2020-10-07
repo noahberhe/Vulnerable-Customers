c08809c4d6f0529ca9d01fa9094bf94a3a5e8ddf
Noah Berhe <noah.berhe@gmail.com>
2020-10-07 13:35:01 +0100

    create example book

--- synonym.py
+++ synonym.py
@@ -12,23 +12,6 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.corpus import wordnet
 
-def retrieve_synsets(word):
-    '''
-    Retrive the list of wordnet synonyms for a given word, and it's definition.
-    
-    Args:
-    word = text string
-    
-    Outputs:
-    list of tuples (synonym name, synonym definition)
-    '''
-    
-    syns = []
-    for i in range(len(wordnet.synsets(word))):
-                syns.append((wordnet.synsets(word)[i].name(),
-                             wordnet.synsets(word)[i].definition()))
-    return syns
-
 
 def word_scorer(w1, w2 = None, with_similarity_score = False):
     '''
@@ -36,30 +19,30 @@ def word_scorer(w1, w2 = None, with_similarity_score = False):
     
     Args:
     w1 = word, text string, 'code'
-    w2 = defined synset for similarity comparison, e.g. 'code.v.01'
+    w2 = defined synset for similarity comparison, e.g. 'code.v.01' (default = None)
+    with_similarity_score = set to True to include similarity scores of w1 with w2 synsets (default = False)
     
     Outputs:
     list of tuples (synonym name, synonym definition, similarity score)
     '''
 
     syns = []
-    w2 = wordnet.synset(w2)
     for i in range(len(wordnet.synsets(w1))):
         if with_similarity_score:
-            if w2:
+            if w2 is not None:
                 syns.append((wordnet.synsets(w1)[i].name(),
                              wordnet.synsets(w1)[i].definition(),
-                             w2.wup_similarity(wordnet.synsets(w1)[i])))
+                             wordnet.synset(w2).wup_similarity(wordnet.synsets(w1)[i])))
             else:
-                break
                 print('with_similarity_score set to True, but no w2 defined')
+                break
         else:
             syns.append((wordnet.synsets(w1)[i].name(),
                          wordnet.synsets(w1)[i].definition()))
     return syns
 
 
-def sentence_cleaner(phrase):
+def prep_phrase(phrase):
     '''
     Removes stopwords, punctuation from text, and converts into a list of word tokens
     
@@ -76,23 +59,28 @@ def sentence_cleaner(phrase):
     return filtered_sentence
 
 
-def topic_scorer(filtered_sentence, topic, sim_thresh = 0.6, return_hits = False):
+def topic_scorer(phrase, topic, sim_thresh = 0.6, return_hits = False):
     '''
     For each word in a sentence, retrieves the synonym set. For each synonym we measure the wup_similarity
     to the topic at hand. If similarity > sim_threshold, the topic is said to have been mentioned.
+    The wup_similarity threshold can be configured: where a higher threshold for increases the strictness of the word-to-topic similarity condition.
+    If return_hits is set to True, the words in the phrase that were mapped to each topic will be returned.
     
     Args:
     filtered_sentence = tokenized sentence, preferrably stripped of stopwords
-    topic = Synset of the topic in question.
-    sim_threshold = threshold for topic similarity (default = 0.6)
+    topic = synset of the topic in question.
+    sim_thresh = wup_similarity threshold for word and topic to be deemed similar enough (default 0.6)
+    return_hits = return the words that matched to each topic (default = False)
     
     Outputs:
     Integer count of the number of mentions of the topic in the filtered_sentence
     '''
     
+    phrase = prep_phrase(phrase)
     word_scores = []
-    for w in range(len(filtered_sentence)):
-        syns = wordnet.synsets(filtered_sentence[w])
+    
+    for w in range(len(phrase)):
+        syns = wordnet.synsets(phrase[w])
         syns_sim = [topic.wup_similarity(syns[synonym]) for synonym in range(len(syns))]
         syns_sim = [sim if sim is not None else 0 for sim in syns_sim]
         try:
@@ -100,7 +88,7 @@ def topic_scorer(filtered_sentence, topic, sim_thresh = 0.6, return_hits = False
         except ValueError:
             syns_sim = 0
         word_scores.append(syns_sim)
-    hits = [filtered_sentence[w] for w in range(len(filtered_sentence)) if word_scores[w] == 1]
+    hits = [phrase[w] for w in range(len(phrase)) if word_scores[w] == 1]
         
     if return_hits:    
         return (np.sum(word_scores), hits)
@@ -108,21 +96,26 @@ def topic_scorer(filtered_sentence, topic, sim_thresh = 0.6, return_hits = False
         return np.sum(word_scores)
 
 
-def phrase_scorer(phrase, topic_dictionary, sim_thresh=0.6, return_hits=False):
+def multi_topic_scorer(phrase, topic_dictionary, sim_thresh=0.6, return_hits=False):
     '''
     Takes a passage of text and maps words in that text to topics that have been defined in a topic dictionary.
+    The wup_similarity threshold can be configured: where a higher threshold for increases the strictness of the word-to-topic similarity condition.
+    If return_hits is set to True, the words in the phrase that were mapped to each topic will be returned.
     
     Args:
     phrase = passage of text
     topic_dictionary = dictionary where key:value is reader-friendly topic name:assigned synonym in wordnet
+    sim_thresh = wup_similarity threshold for word and topic to be deemed similar enough (default 0.6)
+    return_hits = return the words that matched to each topic (default = False)
     
     Outputs:
     sim_scores = dictionary where key:value is the reader-friendly topic name:number of synonyms present in the text
     '''
-    
+
     sim_scores = {}
+    
     for topic in list(topic_dictionary.keys()):
         topic_synset = wordnet.synset(topic_dictionary['{}'.format(topic)])
-        sim_scores['{}'.format(topic)] = synonym_scorer(sentence_cleaner(phrase), topic_synset, sim_thresh, return_hits)
+        sim_scores['{}'.format(topic)] = topic_scorer(phrase, topic_synset, sim_thresh, return_hits)
     return sim_scores
 
